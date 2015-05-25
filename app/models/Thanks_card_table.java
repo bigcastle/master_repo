6b400b364038ad99c3190b5b7afb005486151666
User <User@H27-NotePC07>
2015-05-25 16:55:16 +0900

    各テーブルを追加しました

---
+++ app/models/Thanks_card_table.java
@@ -0,0 +1,17 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Thanks_card_table extends Model {
+
+	@Id
+	public Integer STAFF_ID;
+
+	public Integer THANKS_CARD_ID;
+
+}
