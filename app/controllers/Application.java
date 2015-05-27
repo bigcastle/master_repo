22c0fe5cf93929ee559206b1a315ae70cbf34475
matsumotoyoshihiro <star_light_breaker_extension@yahoo.co.jp>
2015-05-27 16:53:15 +0900

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,16 +20,6 @@ public class Application extends Controller {
 
 
 
-    public static Result caseview(){
-    	List<Card_detail_table> card_list = Card_detail_table.find.all();
-
-    	Card_detail_table card =Card_detail_table.find.byId(1);
-    	Thanks_card_table card_id =Thanks_card_table.find.byId(card.THANKS_CARD_RECEIVE);
-    	Stafftable staff_id =Stafftable.find.byId(card_id.STAFF_ID);
-    	DEPARTMENT_TABLE department_id =DEPARTMENT_TABLE.find.byId(staff_id.DEPARTMENT_ID);
-    	return ok(caseview.render(card_list,card_id));
-    }
-
 
 
 }
