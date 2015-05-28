fcf4b7135a33f43b8b0fadf4b60eb31287e8c189
User <User@H27-NotePC07>
2015-05-28 18:11:25 +0900

    ソートして表示

--- app/controllers/Showcase.java
+++ app/controllers/Showcase.java
@@ -13,8 +13,10 @@ public class Showcase extends Controller {
     public static Result showcase(){
 
     	List<Card_detail_table> card_list = Card_detail_table.find.all();
+    	List<Card_detail_table> ex_card = Card_detail_table.find.all();
+    	ex_card = Card_detail_table.find.where().orderBy("thanks_card_point desc,thanks_card_date desc").findList();
     	//ex_card = Card_detail_table.find.where().orderBy("thanks_card_date").findList();
 
-    	return ok(showcase.render(card_list));
+    	return ok(showcase.render(card_list,ex_card));
     }
 }
