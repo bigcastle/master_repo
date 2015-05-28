4887493fa8082ad64aa7e46725c22a9760ff55a2
User <User@H27-NotePC10>
2015-05-28 18:06:37 +0900

    作成画面

--- app/controllers/Create.java
+++ app/controllers/Create.java
@@ -1,6 +1,7 @@
 package controllers;
 import java.util.List;
 
+import models.Card_detail_table;
 import play.data.Form;
 import play.mvc.*;
 import views.html.Mypage;
@@ -11,6 +12,9 @@ import views.html.thanks_card;
 public class Create extends Controller {
 
 	public static Result thanks_card() {
+	List<Card_detail_table> thanks = Card_detail_table.find.all();
+	Card_detail_table = thanks;
+			Card_detail_table.save();
 	    return ok(thanks_card.render());
 	}
 
@@ -19,7 +23,5 @@ public class Create extends Controller {
     	return ok(Mypage.render());
     }
 
-	public static Result create_card(){
 
-	}
 }
