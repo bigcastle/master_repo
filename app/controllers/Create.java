0a33ef185df2e6d940daffa9c22c715928479a0f
User <User@H27-NotePC07>
2015-05-29 18:25:00 +0900

    事例閲覧と感謝カード作成

---
+++ app/controllers/Create.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+import models.*;
+import views.html.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class Create extends Controller {
+
+    public static Result create(){
+
+    	List<Stafftable> staff_list = Stafftable.find.all();
+    	List<Department_table> department_list = Department_table.find.all();
+    	
+    	
+
+
+    	return ok(create.render(staff_list,department_list));
+    }
+
+
+    public static Result cardcreate(){
+
+    	Map<String,String[]> card =request().body().asFormUrlEncoded();
+
+    	Card_detail_table newcard = new Card_detail_table();
+
+    	newcard.thanks_card_date = card.get("date")[0];
+
+        Integer re_staff =Integer.parseInt(card.get("name")[0]);
+    	Stafftable re_staff2 =Stafftable.find.byId(re_staff);
+    	newcard.receive= re_staff2;
+
+    	Integer se_staff =3;
+    	Stafftable se_staff2 =Stafftable.find.byId(se_staff);
+    	newcard.send=se_staff2;
+
+    	newcard.thanks_card_help = card.get("comment")[0];
+    	newcard.thanks_card_word = card.get("thanks_card")[0];
+
+    	newcard.thanks_card_point = 0;
+    	newcard.thanks_card_totalpoint =0;
+
+    	newcard.save();
+
+
+
+    	//Card_detail_table newcard =Form.form(Card_detail_table.class).bindFromRequest().get();
+    	//newcard.save();
+    	return redirect(routes.Create.cardcreate());
+    }
+
+
+}
