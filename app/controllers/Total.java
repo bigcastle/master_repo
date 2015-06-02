9b355536c7b331eb40cbb1f12ceb040251aa111c
User <User@H27-NotePC10>
2015-06-02 13:34:13 +0900

    total commit

--- app/controllers/Total.java
+++ app/controllers/Total.java
@@ -22,32 +22,48 @@ public class Total extends Controller{
 	public static Result total(){
 		int i =0;
 		int total=0;
-
+		Stafftable staff_id = new Stafftable();
 		List<Card_detail_table> card=Card_detail_table.find.all();
 
 		List<Stafftable> staff = Stafftable.find.all();
 		total =0;
 
-		return ok(evaluation.render(card,staff,total,i));
+		return ok(evaluation.render(card,staff,total,i,staff_id));
 
 
 	}
+
 	public static Result newtotal(){
 		List<Stafftable> staff = Stafftable.find.all();
+
 		Map<String, String[]> params = request().body().asFormUrlEncoded();
 		Integer id = Integer.parseInt(params.get("id")[0]);
 		Stafftable staff_id = Stafftable.find.byId(id);
+
 		int total=0;
 		int i =1;
-		List<Card_detail_table> card = Card_detail_table.find.where().eq("receive",staff_id).findList();
-	//	List<Card_detail_table> total_point = Card_detail_table.find.where().eq("receive",staffpoint).findList();
+
+		Calendar calendar = Calendar.getInstance();
+	    int year = calendar.get(Calendar.YEAR);
+	    String day_years = year+"";
+
+
+
+	    List<Card_detail_table> card = Card_detail_table.find.where().eq("receive",staff_id).like("thanks_card_date ", day_years).findList();
+
 		for(int j=0;j<card.size();j++){
 			total +=card.get(j).thanks_card_point;
 		}
-	//	SqlQuery totalp = Ebean.createSqlQuery("SELECT SUM(thanks_card_point) FROM Card_detail_table");
 
-		return ok(evaluation.render(card,staff,total,i));
+
+
+
+
+		return ok(evaluation.render(card,staff,total,i,staff_id));
 	}
 
 
+
+
 }
+
